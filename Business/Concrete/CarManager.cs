16a411f6213f9a441af29191187fa1b093ffbf2a
ceylann96 <91429246+ceylann96@users.noreply.github.com>
2022-09-06 17:01:55 +0300

    Learning WebAPI layer

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -41,7 +41,7 @@ namespace Business.Concrete
 
         public IResult Add(Car car)
         {
-            if (car.DailyPrice<=0 && car.CarName.Length<2)
+            if (car.DailyPrice <= 0 && car.CarName.Length < 2)
             {
                 return new ErrorResult(Messages.CarNameInvalid);
             }
